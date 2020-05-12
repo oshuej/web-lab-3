6d3f98a6c8516fb2034085cc7f6b4faced9e7905
Nikita <nikitak1324@gmail.com>
2020-05-12 16:51:43 +0300

    fix

--- task2.js
+++ task2.js
@@ -2,13 +2,11 @@
 
 function sum() {
     let sum = 0;
-    for (let argument of arguments) {
-        argument.forEach(arg => {
-            sum += arg;
-        });
-    }
+    this.forEach(num => {
+        sum += num;
+    });
     return sum;
 }
 
 
-console.log(sum.call(null, [5, 5, 5, 5, 5, 5, 5, 5, 5]));
+console.log(sum.call([5, 5, 5, 5, 5, 5, 5, 5, 5]));
